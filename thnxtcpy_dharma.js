bed9daee93d25130268a482e3a7e6a91981c40d5
ddetoni <douglasdetoni92@gmail.com>
2015-09-18 19:04:25 -0300

    Add counter steps.

--- thnxtcpy_dharma.js
+++ thnxtcpy_dharma.js
@@ -1,6 +1,11 @@
+var incStepCounter = function() {
+  Session.set('stepCounter', Session.get('stepCounter') + 1);
+}
+
 if(Meteor.isClient) {
   Session.set('yourName', 'Your Name');
   Session.set('companyName', 'Your Company');
+  Session.set('stepCounter', 1);
 
   Template.form.events({
     'blur .your-name': function(event) {
@@ -10,10 +15,12 @@ if(Meteor.isClient) {
         Session.set('yourName', 'Your Name');
       } else {
         Session.set('yourName', text);
+        incStepCounter();
         $('.step2').removeClass('hide');
       }
     },
     'click .continue-btn': function(event) {
+      incStepCounter();
       $('.step3').removeClass('hide');
       $('.step1').addClass('hide');
       $('.step2').addClass('hide');
@@ -25,6 +32,7 @@ if(Meteor.isClient) {
         Session.set('companyName', 'Your Company');
       } else {
         Session.set('companyName', text);
+        incStepCounter();
         $('.step4').removeClass('hide');
       }
     },
@@ -40,6 +48,7 @@ if(Meteor.isClient) {
         $('.company-type-2more').removeClass('hide');
         $('.company-type').addClass('hide');
       } else {
+        incStepCounter();
         $('.company-type-startup').removeClass('hide');
         $('.company-type').addClass('hide');
       }
@@ -47,6 +56,7 @@ if(Meteor.isClient) {
     'change .disruptive-question': function(event){
       var value = $('.disruptive-question').val();
       if(value === 'yes') {
+        incStepCounter();
         $('.step3').addClass('hide');
         $('.step4').addClass('hide');
         $('.step5').removeClass('hide');
@@ -57,27 +67,33 @@ if(Meteor.isClient) {
     'blur .area-disruption': function(event) {
       var text = $('.area-disruption').val();
       if(text !== '') {
+        incStepCounter();
         Session.set('areaDisruption', text);
         $('.step6').removeClass('hide');
       }
     },
     'change .disruption-type': function(event) {
+      incStepCounter();
       $('.step7').removeClass('hide');
     },
     'change .new-type': function(event) {
+      incStepCounter();
       $('.step8').removeClass('hide');
     },
     'change .range-type': function(event) {
+      incStepCounter();
       $('.step9').removeClass('hide');
     },
     'blur .company-description': function(event) {
       var text = $('.company-description').val();
       if(text !== '') {
+        incStepCounter();
         Session.set('companyDescription', text);
         $('.step10').removeClass('hide');
       }
     },
     'click .continue-2-btn': function(event) {
+      incStepCounter();
       $('.step5').addClass('hide');
       $('.step6').addClass('hide');
       $('.step7').addClass('hide');
@@ -89,6 +105,7 @@ if(Meteor.isClient) {
     'blur .company-url': function(event) {
       var text = $('.company-url').val();
       if(text !== '') {
+        incStepCounter();
         Session.set('companyUrl', text);
         $('.question1').addClass('hide');
         $('.question2').removeClass('hide');
@@ -97,6 +114,7 @@ if(Meteor.isClient) {
     'blur .company-video': function(event) {
       var text = $('.company-video').val();
       if(text !== '') {
+        incStepCounter();
         Session.set('companyVideo', text);
         $('.question2').addClass('hide');
         $('.question3').removeClass('hide');
@@ -105,6 +123,7 @@ if(Meteor.isClient) {
     'blur .company-press': function(event) {
       var text = $('.company-press').val();
       if(text !== '') {
+        incStepCounter();
         Session.set('companyPress', text);
         $('.question3').addClass('hide');
         $('.question4').removeClass('hide');
@@ -113,6 +132,7 @@ if(Meteor.isClient) {
     'blur .company-needs': function(event) {
       var text = $('.company-needs').val();
       if(text !== '') {
+        incStepCounter();
         Session.set('companyNeeds', text);
         $('.question4').addClass('hide');
         $('.question5').removeClass('hide');
@@ -121,6 +141,7 @@ if(Meteor.isClient) {
     'blur .company-help': function(event) {
       var text = $('.company-help').val();
       if(text !== '') {
+        incStepCounter();
         Session.set('companyHelp', text);
         $('.question5').addClass('hide');
         $('.question6').removeClass('hide');
@@ -129,6 +150,7 @@ if(Meteor.isClient) {
     'blur .company-feel': function(event) {
       var text = $('.company-feel').val();
       if(text !== '') {
+        incStepCounter();
         Session.set('companyFeel', text);
         $('.question6').addClass('hide');
         $('.question7').removeClass('hide');
@@ -139,6 +161,7 @@ if(Meteor.isClient) {
       var text2 = $('.company-people-2').val();
       var text3 = $('.company-people-3').val();
       if(text1 !== '' && text2 !== '' && text3 !== '') {
+        incStepCounter();
         Session.set('companyPeople1', text1);
         Session.set('companyPeople2', text2);
         Session.set('companyPeople3', text3);
@@ -151,6 +174,7 @@ if(Meteor.isClient) {
       var text2 = $('.company-people-2').val();
       var text3 = $('.company-people-3').val();
       if(text1 !== '' && text2 !== '' && text3 !== '') {
+        incStepCounter();
         Session.set('companyPeople1', text1);
         Session.set('companyPeople2', text2);
         Session.set('companyPeople3', text3);
@@ -163,6 +187,7 @@ if(Meteor.isClient) {
       var text2 = $('.company-people-2').val();
       var text3 = $('.company-people-3').val();
       if(text1 !== '' && text2 !== '' && text3 !== '') {
+        incStepCounter();
         Session.set('companyPeople1', text1);
         Session.set('companyPeople2', text2);
         Session.set('companyPeople3', text3);
@@ -173,6 +198,7 @@ if(Meteor.isClient) {
     'blur .company-important': function(event) {
       var text = $('.company-important').val();
       if(text !== '') {
+        incStepCounter();
         Session.set('companyImportant', text);
         $('.question8').addClass('hide');
         $('.finalStepYes').removeClass('hide');
@@ -186,6 +212,12 @@ if(Meteor.isClient) {
     },
     companyName: function() {
       return Session.get('companyName');
+    }
+  });
+
+  Template.stepCounter.helpers({
+    counter: function() {
+      return Session.get('stepCounter');
     }
   });
 }
